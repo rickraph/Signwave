02b0264b86c0b138c32b37bb6532834ede04b07b
rickraph <ricksonraphelofficial@gmail.com>
2024-12-23 09:31:18 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,111 @@
+import speech_recognition as sr
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+from easygui import *
+import os
+from PIL import Image, ImageTk
+from itertools import count
+import tkinter as tk
+import string
+
+def func():
+    r = sr.Recognizer()
+    with open('sentences.txt', 'r') as file:
+        isl_gif = file.read().splitlines()
+
+    arr=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r', 's','t','u','v','w','x','y','z']
+    
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source) 
+        while True:
+            print("I am Listening")
+            audio = r.listen(source)
+            try:
+                a=r.recognize_google(audio)
+                a = a.lower()
+                print('You Said: ' + a.lower())
+
+                for c in string.punctuation:
+                    a= a.replace(c,"")
+
+                if(a.lower()=='goodbye' or a.lower()=='good bye' or a.lower()=='bye'):
+                    print("oops!Time To say good bye")
+                    break
+
+                elif(a.lower() in isl_gif):
+
+                    class ImageLabel(tk.Label):
+                        """a label that displays images, and plays them if they are gifs"""
+                        def load(self, im):
+                            if isinstance(im, str):
+                                im = Image.open(im)
+                            self.loc = 0
+                            self.frames = []
+
+                            try:
+                                for i in count(1):
+                                    self.frames.append(ImageTk.PhotoImage(im.copy()))
+                                    im.seek(i)
+                            except EOFError:
+                                pass
+
+                            try:
+                                self.delay = im.info['duration']
+                            except:
+                                self.delay = 100
+
+                            if len(self.frames) == 1:
+                                self.config(image=self.frames[0])
+                            else:
+                                self.next_frame()
+
+                        def unload(self):
+                            self.config(image=None)
+                            self.frames = None
+
+                        def next_frame(self):
+                            if self.frames:
+                                self.loc += 1
+                                self.loc %= len(self.frames)
+                                self.config(image=self.frames[self.loc])
+                                self.after(self.delay, self.next_frame)
+
+                    root = tk.Tk()
+                    lbl = ImageLabel(root)
+                    lbl.pack()
+                    lbl.load(r'ISL_Gifs/{0}.gif'.format(a.lower()))
+                    root.mainloop()
+
+                elif(a.lower() == 'add new sentence'):
+                    new_sentence = input("Enter the new sentence: ")
+                    new_gif = input("Enter the name of the new gif (without .gif extension): ")
+                    with open('sentences.txt', 'a') as file:
+                        file.write(new_sentence + '\n')
+                    isl_gif.append(new_gif.lower())
+
+                else:
+                    for i in range(len(a)):
+                        if(a[i] in arr):
+                            ImageAddress = 'letters/'+a[i]+'.jpg'
+                            ImageItself = Image.open(ImageAddress)
+                            ImageNumpyFormat = np.asarray(ImageItself)
+                            plt.imshow(ImageNumpyFormat)
+                            plt.draw()
+                            plt.pause(0.8)
+                        else:
+                            continue
+
+            except:
+                print(" ")
+            plt.close()
+
+while 1:
+  image   = "signlang.png"
+  msg="HEARING IMPAIRMENT ASSISTANT"
+  choices = ["Live Voice","All Done!"] 
+  reply   = buttonbox(msg,image=image,choices=choices)
+  if reply ==choices[0]:
+        func()
+  if reply == choices[1]:
+        quit()
